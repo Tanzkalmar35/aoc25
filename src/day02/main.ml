a4647c5deeb586588bb3c4ff6c11dd96bbbf9dc5
Fabian Holler <fabianholler2005@gmail.com>
2025-12-03 21:32:36 +0000

    day02 part 1 done

--- src/day02/main.ml
+++ src/day02/main.ml
@@ -21,46 +21,57 @@ let split_number n =
   
   (int_of_string left_str, int_of_string right_str)
 
+let sum l =
+  let (+) a b =
+    match (a,b) with
+      | (None,x) | (x,None) -> x
+      | (Some x,Some y)     -> Some (x+y)
+  in
+  let convert a = Some a in
+  let opt_l=List.map convert l  in
+  List.fold_left (+) None opt_l
+
+let flatten lst =
+  let rec aux lst acc = match lst with
+    | [] -> acc
+    | x :: xs -> aux xs (acc @ x)
+  in aux lst []   
+
 let parse_line line =
   Scanf.sscanf line "%d-%d" (fun start stop ->
       let rec iter_print num acc = 
           if num >= stop + 1 then 
               acc
           else begin          
-              Printf.printf "%d," num;
-              
-              (* 1. Convert to string once so we can check length *)
               let s_num = string_of_int num in
               let len = String.length s_num in
 
               let new_acc = 
-                  (* 2. CHECK: Only proceed if length is even *)
                   if len mod 2 = 0 then begin
-                      (* It is safe to split now *)
-                      (* Assuming split_number takes an int, pass 'num' back to it *)
-                      (* If your split_number takes a string, pass 's_num' *)
                       let (first_half, second_half) = split_number s_num in 
                       
-                      if first_half = second_half then
+                      if first_half = second_half then begin
                           num :: acc
-                      else 
+                      end else 
                           acc
                   end
                   else 
-                      (* Length is odd: skip logic, return accumulator unchanged *)
                       acc
               in
               
               iter_print (num + 1) new_acc
           end                 
       in
-      let res = iter_print start [] in
-      Printf.printf " (Invalid count: %d)\n" (List.length res)
+      iter_print start []
   )
+    
+
 (* The Solver Logic for part 1*)
 let solve_part_1 lines =
-    lines
-    |> List.iter parse_line
+    let l = lines
+      |> List.map parse_line in
+
+    sum (flatten l)
 
 (* let solve_part_2 lines = *)
 (*     lines List.iter *)
@@ -76,8 +87,11 @@ let () =
   let lines = read_lines filename in
   
   (*Part 1*)
-  solve_part_1 lines
-  (**)
+  let res = solve_part_1 lines in
+  match res with
+  | Some sum -> Printf.printf "[Part 1] Final sum: %d\n" sum;
+  | None -> Printf.eprintf "[Part 1]: Error summing everything up \n";
+
   (* let (final_pos, final_count) = solve_part_2 lines in *)
   (* Printf.printf "[Part 2] Final Position: %d\n" final_pos; *)
   (* Printf.printf "[Part 2] Final Result: %s\n" (string_of_int final_count) *)
